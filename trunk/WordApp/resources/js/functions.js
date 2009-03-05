3098b98e2c6aab104f97f7c66ff99eb57c9417fd
mytwinsen <mytwinsen@64e79ce5-f150-0410-b22f-0d57465ade6a>
2009-03-05 15:00:55 +0000

    added stub add word

--- trunk/WordApp/resources/js/functions.js
+++ trunk/WordApp/resources/js/functions.js
@@ -4,25 +4,30 @@
  * @param fileName
  * @return loaded xml object from given file 
  */
-function createXMLObject(fileName) {
-	//	 Internet Explorer
-	try
-	{
-		xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
-	} catch (e) {
-		//	 Firefox, Mozilla, Opera, etc.
-		try
-		{
-			xmlDoc = document.implementation.createDocument("", "", null);
-		} catch (e) {
-			alert(e.message);
-			return false;
-		}
-	}
-	xmlDoc.async = false;
-	xmlDoc.load("../resources/data/"+fileName);
-	return xmlDoc;
+function loadXMLDoc(fileName) 
+{
+try //Internet Explorer
+  {
+  xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+  }
+catch(e)
+  {
+  try //Firefox, Mozilla, Opera, etc.
+    {
+    xmlDoc=document.implementation.createDocument("","",null);
+    }
+  catch(e) {alert(e.message)}
+  }
+try 
+  {
+  xmlDoc.async=false;
+  xmlDoc.load("../resources/data/"+fileName);
+  return(xmlDoc);
+  }
+catch(e) {alert(e.message)}
+return(null);
 }
+
 
 /**
  * Function gets a random word text from xml file
@@ -31,7 +36,7 @@ function createXMLObject(fileName) {
  */
 function doStart() {
 	 
-	var xmlDoc = createXMLObject("LVEN.xml");
+	var xmlDoc = loadXMLDoc("LVEN.xml");
 	
 	var nodes = xmlDoc.getElementsByTagName("word");
 	//	Generate random index depends on xml nodes length
@@ -55,7 +60,7 @@ function checkAnswer() {
 	var question = document.getElementById("question").innerHTML;
 	var answer = document.getElementById("answer").value;
 	
-	var xmlDoc = createXMLObject("LVEN.xml");
+	var xmlDoc = loadXMLDoc("LVEN.xml");
 	var nodes = xmlDoc.getElementsByTagName("word");
 	
 	for (i=0; i<nodes.length; i++) {
@@ -84,10 +89,49 @@ function doAnswer() {
 	doStart();
 	//	clear all user input text
 	document.getElementById("answer").value = "";
+	
+	//	Focus on input
+	document.getElementById("answer").focus();
 }
 
+/**
+ * Function add new word node to xml
+ * 
+ * @return
+ */
 function addNewWord() {
-
+	var text = document.getElementById("text").value;
+	var translation = document.getElementById("translation").value;
+	
+	var xmlDoc = loadXMLDoc("LVEN.xml");	
+	var nodes = xmlDoc.getElementsByTagName("word");	
+	for (i=0; i<nodes.length; i++) {		
+		if(nodes[i].getElementsByTagName("text")[0].childNodes[0].nodeValue == text) {
+			document.getElementById("notif").innerHTML = "";
+			document.getElementById("err").innerHTML = "Data already exists!";
+			return;
+		}
+	}
+	if(text != "" && translation != "") {
+		
+//		var newWord = xmlDoc.createElement("word");
+//		var newText = xmlDoc.createElement("text");
+//		var newTextData = xmlDoc.createTextNode(text);
+//		newWord.appendChild(newText.appendChild(newTextData));
+//		
+//		var newTrans=xmlDoc.createElement("translation");
+//		var newTransData = xmlDoc.createTextNode(translation);
+//		newWord.appendChild(newTrans.appendChild(newTransData));		
+		
+//		xmlDoc.appendChild(newWord);
+		
+		document.getElementById("err").innerHTML = "";
+		document.getElementById("notif").innerHTML = "Data added sucessfuly";
+	} else {
+		document.getElementById("notif").innerHTML = "";
+		document.getElementById("err").innerHTML = "Please provide data";
+	}
+	
 }
 
 function viewData() {
